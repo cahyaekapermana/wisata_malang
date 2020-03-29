952e6d2ee4aa947a1df5db08ddf8eaa97d907523
cahyaekapermana <cahyaekapermana@gmail.com>
2020-03-29 21:32:28 +0700

    DESAIN UI LOGIN

--- app/src/main/java/com/example/wisata_malang/MainActivity.java
+++ app/src/main/java/com/example/wisata_malang/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.wisata_malang;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    //Exit Button
+    public void handleExit(View view) {
+        finish();
+        System.exit(0);
+    }
+
+    public void handleMasuk(View view) {
+        
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
     }
 }
